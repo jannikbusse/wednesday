280d3b422a71b4b0cad38818e79ea306a3a146fb
Jannik Busse <jannik.busse@rwth-aachen.de>
2020-11-12 16:20:17 +0100

    ja

--- __init__.py
+++ __init__.py
@@ -1,13 +1,17 @@
 from flask import Flask
 import datetime
 
+counter = 0
+
 app = Flask(__name__)
 weekdays = ["Monday :(", "Tuesday :|", "WEDNESDAY MY DUDES!!", "Thursday ;(", "Friday", "Saturday", "Sunday"]
 
 
 @app.route("/")
 def home():
-    return "It is "  + weekdays[datetime.datetime.today().weekday()]
+    global counter
+    counter += 1
+    return "<h2>It is "  + weekdays[datetime.datetime.today().weekday()] + "</h2>counter since last update:" + str(counter)
 
 if __name__ == "__main__":
     app.run()
